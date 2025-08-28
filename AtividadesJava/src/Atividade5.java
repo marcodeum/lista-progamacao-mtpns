5704cc5f723e2a4577ebe1dfd74ce92a49d21b07
marcodeum <marco.souza300322@gmail.com>
2025-08-28 19:40:22 -0300

    Atividade5 - resolvida

---
+++ AtividadesJava/src/Atividade5.java
@@ -0,0 +1,6 @@
+public class Atividade5 {
+    public static void main(String[] args) {
+        String curso = "Técnico de Informática"; int cargaHoraria = 8;
+        System.out.println("Curso: "+curso+" - Carga horária: "+cargaHoraria);
+    }
+}
