2626333516c3b4b6b06450c6fe2485d0e85cfe7c
marcodeum <marco.souza300322@gmail.com>
2025-08-28 19:33:01 -0300

    Atividade3 - resolvida

---
+++ AtividadesJava/src/Atividade3.java
@@ -0,0 +1,9 @@
+public class Atividade3 {
+    public static void main(String[] args) {
+        int qtdAlunos = 25; double mediaTurma = 60; boolean aprovada = true; String professor = "Vitor";
+        System.out.println("Quantidade de alunos: "+qtdAlunos);
+        System.out.println("Média da turma: "+mediaTurma);
+        System.out.println("Turma aprovada?: "+aprovada);
+        System.out.println("Professor da turma: "+professor);
+    }
+}
