2a8708808d29da1723e06757c525ddc90884944a
marcodeum <marco.souza300322@gmail.com>
2025-08-28 19:28:19 -0300

    Atividade2 - resolvida

---
+++ AtividadesJava/src/Atividade2.java
@@ -0,0 +1,9 @@
+public class Atividade2 {
+    public static void main(String[] args) {
+        int dia = 30; int mes = 03; int ano = 2009;
+        System.out.println("Data de nascimento:");
+        System.out.println("Dia: "+dia);
+        System.out.println("Mês: "+mes);
+        System.out.println("Ano: "+ano);
+    }
+}
