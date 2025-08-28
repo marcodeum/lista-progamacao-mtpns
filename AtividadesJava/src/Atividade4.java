1171476eb177f88f8bc8169de2741e1a2ce13061
marcodeum <marco.souza300322@gmail.com>
2025-08-28 19:51:22 -0300

    Atividade4 - corrigida

--- AtividadesJava/src/Atividade4.java
+++ AtividadesJava/src/Atividade4.java
@@ -1,6 +1,6 @@
 public class Atividade4 {
     public static void main(String[] args) {
-        final double PI = 3.1415; int raio = 5;
+        final double PI = 3.1415; double raio = 5;
         System.out.println("PI = "+PI);
         System.out.println("Raio = "+raio);
     }
