0e1a6bf72a311337dd35a7e74335076ca4c92709
marcodeum <marco.souza300322@gmail.com>
2025-08-28 19:35:42 -0300

    Atividade4 - resolvida

---
+++ AtividadesJava/src/Atividade4.java
@@ -0,0 +1,7 @@
+public class Atividade4 {
+    public static void main(String[] args) {
+        final double PI = 3.1415; int raio = 5;
+        System.out.println("PI = "+PI);
+        System.out.println("Raio = "+raio);
+    }
+}
