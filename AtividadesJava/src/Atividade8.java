ff8fa081168836a1a822d5312155b8846bb6cb26
marcodeum <marco.souza300322@gmail.com>
2025-08-28 19:56:28 -0300

    Atividade9 - resolvida

--- AtividadesJava/src/Atividade8.java
+++ AtividadesJava/src/Atividade8.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Atividade8 {
+    public static void main(String[] args) {
+        int nota1 = 10; int nota2 = 15; int nota3 = 20;
+        System.out.println("A média das notas é: "+(nota1+nota2+nota3)/3);
+    }
 }
