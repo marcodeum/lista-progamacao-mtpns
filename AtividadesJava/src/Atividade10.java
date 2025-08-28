6ac70ce31b07f8f34062d3a42a3950c045e30466
marcodeum <marco.souza300322@gmail.com>
2025-08-28 20:00:47 -0300

    Atividade10 - resolvida

---
+++ AtividadesJava/src/Atividade10.java
@@ -0,0 +1,6 @@
+public class Atividade10 {
+    public static void main(String[] args) {
+        double celsius = 37.5; double fahrenheint = (9 * celsius) / 5 + 32;
+        System.out.println(fahrenheint);
+    }
+}
