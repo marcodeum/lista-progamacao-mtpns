e5fb9f3928afb3f4cf1be734d22577f0f1a77db3
marcodeum <marco.souza300322@gmail.com>
2025-08-28 19:46:23 -0300

    Atividade6 - resolvida

---
+++ AtividadesJava/src/Atividade6.java
@@ -0,0 +1,6 @@
+public class Atividade6 {
+    public static void main(String[] args) {
+        int idade = 16; final int meses_ano = 12;
+        System.out.println("Sua idade em meses é: "+(idade*meses_ano));
+    }
+}
