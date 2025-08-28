95770f5774a623a93880e7d6dee40416e38549c3
marcodeum <marco.souza300322@gmail.com>
2025-08-28 19:59:08 -0300

    Atividade9 - resolvida

---
+++ AtividadesJava/src/Atividade9.java
@@ -0,0 +1,6 @@
+public class Atividade9 {
+    public static void main(String[] args) {
+        int base = 10; int altura = 20; int perimetro = (2 * (base+altura));
+        System.out.println(perimetro);
+    }
+}
