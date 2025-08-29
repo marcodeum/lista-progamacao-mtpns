d15cb64b5ebbed6972235c5b1042d0f357bcff86
marcodeum <marco.souza300322@gmail.com>
2025-08-28 21:10:15 -0300

    Atividade8 - corrigida

--- AtividadesJava/src/Atividade8.java
+++ AtividadesJava/src/Atividade8.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Atividade8 {
     public static void main(String[] args) {
-        int nota1 = 10; int nota2 = 15; int nota3 = 20;
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite o valor da nota1");
+        int nota1 = leia.nextInt();
+        System.out.println("Digite o valor da nota2:");
+        int nota2 = leia.nextInt();
+        System.out.println("Digite o valor da nota3");
+        int nota3 = leia.nextInt();
         System.out.println("A média das notas é: "+(nota1+nota2+nota3)/3);
     }
 }
