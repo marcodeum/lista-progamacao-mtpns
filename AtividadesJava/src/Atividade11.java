99341f64545bd408473f6934813062778243d111
marcodeum <marco.souza300322@gmail.com>
2025-08-28 21:21:25 -0300

    Atividade11 - corrigida

--- AtividadesJava/src/Atividade11.java
+++ AtividadesJava/src/Atividade11.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Atividade11 {
     public static void main(String[] args) {
-        int idadeAnos = 16; int idadeMeses = (12 * idadeAnos);
-        System.out.println(idadeMeses);
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite sua idade:");
+        int idadeAnos = leia.nextInt();
+        int idadeMeses = idadeAnos*12;
+        System.out.println("Sua idade em meses é:" +idadeMeses);
     }
 }
