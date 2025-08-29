249fa30a6d5c6e13a48c433f36a1dee47e635621
marcodeum <marco.souza300322@gmail.com>
2025-08-28 21:39:45 -0300

    Atividade14 - resolvida

---
+++ AtividadesJava/src/Atividade14.java
@@ -0,0 +1,10 @@
+public class Atividade14 {
+    public static void main(String[] args) {
+        int a = 20; int b = 5; int soma = a+b; int subtracao = a-b; int mult = a*b; int quociente = a/b; int resto = a%b;
+        System.out.println("a+b: "+soma);
+        System.out.println("a-b: "+subtracao);
+        System.out.println("a*b: "+mult);
+        System.out.println("a/b: "+quociente);
+        System.out.println("a%b: "+resto);
+    }
+}
