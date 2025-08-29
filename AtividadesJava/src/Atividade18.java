a0a3558e227a5a00494cb84eb36fb06c9b3627c4
marcodeum <marco.souza300322@gmail.com>
2025-08-28 21:53:26 -0300

    Atividade18 - resolvida

---
+++ AtividadesJava/src/Atividade18.java
@@ -0,0 +1,15 @@
+public class Atividade18 {
+    public static void main(String[] args) {
+        int p = 18; int q = 13;
+        if (p > q){
+            System.out.println("O valor de p é maior que o de q");
+        }
+        if (p == q){
+            System.out.println("O valor de p é igual ao valor de q");
+        }
+        if (p < q){
+            System.out.println("O valor de p é menor que o valor de q");
+        }
+        }
+    }
+
