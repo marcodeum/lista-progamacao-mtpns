d9032bcb238a114d94e9d79fafa1eab0272baaa6
marcodeum <marco.souza300322@gmail.com>
2025-08-28 21:15:58 -0300

    Atividade10 - corrigida

--- AtividadesJava/src/Atividade10.java
+++ AtividadesJava/src/Atividade10.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Atividade10 {
     public static void main(String[] args) {
-        double celsius = 37.5; double fahrenheint = (9 * celsius) / 5 + 32;
-        System.out.println(fahrenheint);
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite o valor de celsius:");
+        double celsius = leia.nextDouble();
+        double fahrenheint = (9 * celsius) / 5 + 32;
+        System.out.println("O valor celsius em fahrenheint é: "+fahrenheint);
     }
 }
